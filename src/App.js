09f774d1a7f0610150d3393a6482ea17d4df111e
Gabriel Pope <darwin@Gabriels-MacBook-Air.local>
2022-02-09 17:43:50 -0800

    Weather Data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,39 @@ import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      temperature: 0,
+    }
+
+    this.getWeatherData = this.getWeatherData.bind(this);
   }
 componentDidMount() {
 console.log("Page did load")
+this.getWeatherData();
 }
   //print
+getWeatherData = () => {
+
+  const self = this;
+  axios ({
+    url:  "https://api.openweathermap.org/data/2.5/weather?q=Las+Vegas&appid=83b7f1bab3d05e248e5d700f3abc725d&units=imperial",
+    method: "GET" 
+  })
+  .then(function(results){
+    console.log(results);
+    const weather = results.data.main.temp;
+
+    self.setState({
+      temperature:
+      weather
+    })
+  })
+  .catch(function(error){
+   console.log(error);
+  })  
+   
+}
 
   render() {
     return (
@@ -27,6 +55,11 @@ console.log("Page did load")
           >
             Learn React
           </a>
+          <p>
+            temperature: {this.state.temperature
+
+            }
+          </p>
         </header>
       </div>
     );
